ade81f8b8d47a9d26a96a3f40f824f07bd4c2176
Bijil Subhash <bijil.subhash@gmail.com>
2025-08-07 22:29:42 +1000

    adding setup.py

--- src/dagster_dlt_demo/definitions.py
+++ src/dagster_dlt_demo/definitions.py
@@ -1,6 +1,5 @@
 import dagster as dg
 
-from dagster_dlt_demo.defs.jobs import dummy_job
 from dagster_dlt_demo.defs.sensor import sensors
 from dagster_dlt_demo.defs.SFTPIngestor.ingestor import orders_assets, customers_assets, products_assets
 
