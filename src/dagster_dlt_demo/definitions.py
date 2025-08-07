ace4574a279a0e4863c5e204211ac9c961c8e4e9
Bijil Subhash <bijil.subhash@gmail.com>
2025-08-07 21:32:12 +1000

    adding setup.py

--- src/dagster_dlt_demo/definitions.py
+++ src/dagster_dlt_demo/definitions.py
@@ -4,7 +4,7 @@ from dagster_dlt_demo.defs.jobs import dummy_job
 from dagster_dlt_demo.defs.sensor import sensors
 
 defs = dg.Definitions(
-    assets=dg.load_assets_from_package_name("dagster_dlt_demo.defs")
+    assets=dg.load_assets_from_package_name("dagster_dlt_demo.defs"),
     jobs = [dummy_job],
     sensors=sensors
 )
