7ce62e5c60774ad47163c5e2d0111931efc8112f
Bijil Subhash <bijil.subhash@gmail.com>
2025-08-07 21:52:39 +1000

    adding setup.py

--- src/dagster_dlt_demo/definitions.py
+++ src/dagster_dlt_demo/definitions.py
@@ -5,7 +5,10 @@ from dagster_dlt_demo.defs.sensor import sensors
 from dagster_dlt_demo.defs.SFTPIngestor.ingestor import orders_assets, customers_assets, products_assets
 
 defs = dg.Definitions(
-    assets=[dg.load_assets_from_package_name("dagster_dlt_demo.defs"), orders_assets, customers_assets, products_assets]
+    assets=[
+        dg.load_assets_from_package_name("dagster_dlt_demo.defs"), 
+        orders_assets, 
+        customers_assets, products_assets],
     jobs = [dummy_job],
     sensors=sensors
 )
